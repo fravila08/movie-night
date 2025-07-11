1453c6df5e94ed615649938dfecbd9e91ce7e8c4
fravila08 <fr4v1l4@gmail.com>
2025-07-11 01:30:03 -0700

    deployment attempt 2

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "./App";
 import UserPage from "./pages/UserPage";
 import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
 import { getGenreFilms } from "./utilities";
 
-export const router = createBrowserRouter([{
+export const router = createHashRouter([{
     path:'/movie-night/',
     element:<App/>,
     children:[
